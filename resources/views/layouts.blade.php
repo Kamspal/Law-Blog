572dd4426aab0c2ba646dc9a5e947e0e4bb6cdd5
KamanChandraPal <kams.pal014@gmail.com>
2020-06-07 12:18:56 +0545

    started new project

---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stiff Rowlands</title>
+
+    <link rel="stylesheet" href="/css/new.css">
+</head>
+<body>
+    <div class="kams">
+    my name is kamspal....!!!!
+    </div>
+</body>
+</html>
